00c2d96cff5ada26da858a1b0a579e38b59b0af3
于建峰 <yujianfeng@jf180.cn>
2016-11-28 18:52:26 +0800

    初始化新题

---
+++ src/cn/leo/easy/N6ZigZagConversion.java
@@ -0,0 +1,29 @@
+package cn.leo.easy;
+
+public class N6ZigZagConversion {
+
+    public static void main(String[] args) {
+        String string = "PAYPALISHIRING";
+        string = convert(string, 3);
+        System.out.println(string);
+    }
+
+    private static String convert(String s, int numRows) {
+        StringBuilder[] sBuilders = new StringBuilder[numRows];
+        for(int i=0;i<numRows;i++){
+            sBuilders[i]=new StringBuilder();
+        }
+        
+        
+        for(int i=0;i<s.length();i++){
+            sBuilders[i%3].append(s.charAt(i));
+        }
+        
+        StringBuilder result=new StringBuilder();
+        for(StringBuilder sBuilder:sBuilders){
+            result.append(sBuilder);
+        }
+
+        return result.toString();
+    }
+}
